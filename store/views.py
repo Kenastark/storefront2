6e961d3c9cec5004a66611f51b729e7d7a6f07ff
kenastark <ikennaudeani@gmail.com>
2023-08-18 07:38:54 +0100

    added error handling for when product does not exists

--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
+
 
 
 # Create your views here.
@@ -13,4 +17,12 @@ def product_list(request):
 
 @api_view()
 def product_detail(request, id):
-    return Response(id)
+    try:
+        product = Product.objects.get(pk=id)
+        # here we create a serializer and give it the product object which converts it to a python dictionary 
+        serializer = ProductSerializer(product)
+        #serializer.data gives us the dictionary
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
