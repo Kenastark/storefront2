b7a1cf5355429f8cbd1aa1d4484270effff6e9e6
kenastark <ikennaudeani@gmail.com>
2023-08-18 07:17:41 +0100

    fixed syntax error in serializers

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-class ProductSerializer(serializers.Serializers):
+class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalFields(max_digits=6, decimal_places=2)
+    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
