e4b9d197ad8e2fa0909fddf916fc35a38dc2551d
kenastark <ikennaudeani@gmail.com>
2023-08-24 13:51:43 +0100

    serialized a relationship using nested object

--- store/serializers.py
+++ store/serializers.py
@@ -2,13 +2,17 @@ from decimal import Decimal
 from store.models import Product, Collection
 from rest_framework import serializers
 
+class CollectionSerializer(serializers.Serializer):
+    id =serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     #SerializerMethodField a method will be defined and will return the result for this field
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.StringRelatedField()
+    collection = CollectionSerializer()
 
     #type annotation is used to enable intellisense
     def calculate_tax(self, product: Product):
