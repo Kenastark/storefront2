3295af1048f8da8fff8ee8512128e8a43e31bccf
kenastark <ikennaudeani@gmail.com>
2023-08-24 13:15:13 +0100

    returned a related field PK

--- store/serializers.py
+++ store/serializers.py
@@ -1,14 +1,18 @@
 from decimal import Decimal
-from store.models import Product
+from store.models import Product, Collection
 from rest_framework import serializers
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+    #SerializerMethodField a method will be defined and will return the result for this field
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    collection = serializers.PrimaryKeyRelatedField(
+        queryset = Collection.objects.all()
+    )
 
-
+    #type annotation is used to enable intellisense
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
      
