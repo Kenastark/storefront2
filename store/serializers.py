9730bdea405a9675042c2a98c845c688d9f0ffec
kenastark <ikennaudeani@gmail.com>
2023-09-07 22:31:58 +0100

    14 - Data Validation

--- store/serializers.py
+++ store/serializers.py
@@ -12,12 +12,19 @@ class ProductSerializer(serializers.ModelSerializer):
 
     # #SerializerMethodField a method will be defined and will return the result for this field
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.HyperlinkedRelatedField(
+    """ collection = serializers.HyperlinkedRelatedField(
         queryset =Collection.objects.all(),
         view_name='collection-detail'
-    )
+    ) """
 
     #type annotation is used to enable intellisense
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
      
+
+# Overriding the validate method in our serializer
+    # def validate(self, data):
+    #     if data['passowrd'] != data['confirm password']:
+    #         return serializers.ValidationError('Passwords do not match')
+        
+    #     return data
